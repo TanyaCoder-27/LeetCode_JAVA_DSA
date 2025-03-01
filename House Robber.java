0003ea273c7d8a08634f2ecedc321cb93448a802
ANDRA TULASI LAKSHMI TANYA <tanyakonapala@gmail.com>
2025-03-01 13:56:16 +0530

    Update House Robber.java

--- House Robber.java
+++ House Robber.java
@@ -27,6 +27,8 @@ Constraints:
 0 <= nums[i] <= 400
 */
 
+//Memoization
+
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
@@ -45,5 +47,32 @@ class Solution {
         int not_pick = 0 + comp(i-1,arr,dp);
 
         return dp[i]=Math.max(pick,not_pick);
+    }
+}
+
+
+// Tabulation with Space Optimization
+
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        int prev = nums[0];
+        int prev2 = 0;
+
+        for(int i=1;i<n;i++){
+
+            int pick = nums[i];
+            if(i>1){
+                pick+= prev2;
+            }
+
+            int not_pick = 0 + prev;
+            int curr = Math.max(pick,not_pick);
+
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
     }
 }
